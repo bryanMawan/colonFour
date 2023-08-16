928ad852e93d62f9c8fab1252588f9f1a6d764e8
bryan <bb222qj@student.lnu.se>
2023-08-16 19:33:19 +0200

    ADJUSTED FONT

--- colonFourApp/urls.py
+++ colonFourApp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import TestView, login_view
+from .views import TestView, login_view, logout_view
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', TestView.as_view(), name='home'),
     path('login/', login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', logout_view, name='logout'),
     ]
